87cdb9a7d1050504bd3d740f733e02089a30791d
yud0uhu <cist.b219.y.osaka@gmail.com>
2023-02-22 21:54:14 +0900

    feat: wasmでブラウザ上からrustコードを呼び出すための初期構成を作成

---
+++ app.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <NuxtWelcome />
+  </div>
+</template>
+<script lang="ts">
+import { defineComponent, ref } from "vue";
+import init, { greet } from "markdown-perser";
+
+export default defineComponent({
+  name: "App",
+  components: {},
+  setup() {
+    init().then(() => {
+      console.log("init wasm-pack");
+      greet("from vite!");
+    });
+  },
+});
+</script>
---
+++ vite.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from "vite";
+import wasmPack from "vite-plugin-wasm-pack";
+
+export default defineConfig({
+  build: {
+    minify: false,
+  },
+  // pass your local crate path to the plugin
+  plugins: [wasmPack("./markdown-perser")],
+});
