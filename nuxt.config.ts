fd8eb36f98428360e7dd20680c281aa5f095b6b1
yud0uhu <cist.b219.y.osaka@gmail.com>
2023-03-08 09:13:07 +0900

    :+1: tailwind,daisyUIのセットアップ

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,2 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-export default defineNuxtConfig({});
+export default defineNuxtConfig({
+  modules: ["@nuxtjs/tailwindcss", "@nuxtjs/color-mode"],
+  colorMode: {
+    preference: "system", // default theme
+    dataValue: "theme", // activate data-theme in <html> tag
+    classSuffix: "",
+  },
+});
